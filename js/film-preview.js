4cdedd6b25b22ff7a401872507348b2f96dffd74
VildeAvloes <vildeavlos@gmail.com>
2023-02-21 14:22:42 +0100

    added js to preview

--- js/film-preview.js
+++ js/film-preview.js
@@ -1,76 +1,47 @@
 import { displayMessage } from "./components/message.js";
 
-{
-  /* <div class="film-title">
-        <h1>Stairway To Heaven</h1>
-        <h2>By Zed Lepplin</h2>
-        <h3>Drama</h3>
-      </div>
-    
-      <div class="film-wrapper">
-        <img src="images/stairwaytoheaven-hq.jpg" alt="Cover photo of the movie: Stairway to heaven. A picture of an old man walking up stairs on a sky." class="film-image" />
-        <div class="container film-info">
-          <div class="ratings">
-            <div>
-              <i class="fa-solid fa-star star"></i>
-              <i class="fa-solid fa-star star"></i>
-              <i class="fa-solid fa-star star"></i>
-              <i class="fa-solid fa-star star"></i>
-              <i class="fa-solid fa-star-half-stroke star"></i>
-            </div>
-          </div>
-          <p>In this film we follow a mans last days on earth. We get to know his story, his achievements and regrets before he takes the stairway to heaven.</p>
-          <div class="button-wrapper__film">
-            <a href="checkout.html" class="cta">Watch Now</a>
-          </div>
-        </div>
-      </div>*/
-}
-
 const detailsContainer = document.querySelector(".film-details");
-const queryString = document.location.search;
-const params = new URLSearchParams(queryString);
-const id = params.get("id");
-console.log(id);
+// const queryString = document.location.search;
+// const params = new URLSearchParams(queryString);
+// const id = params.get("id");
+// console.log(id);
 
-const url = "https://vilde-avlos.no/square-eyes/wp-json/wc/store/products/" + id;
+const url = "https://vilde-avlos.no/square-eyes/wp-json/wc/store/products/25";
 
 console.log(url);
 
-async function getFilm() {
+async function getFilmDetails() {
   try {
     const response = await fetch(url);
-    const filmDetails = await response.json();
+    const details = await response.json();
 
-    console.log(filmDetails);
+    console.log(details);
 
-    createHTML(filmDetails);
+    createHTML(details);
   } catch (error) {
+    console.log(error);
     detailsContainer.innerHTML = displayMessage("error", "Oh no, couldn't fetch film details..");
   }
 }
 
-getFilm();
+getFilmDetails();
 
-function createHTML(filmDetails) {
-  filmDetails.forEach(function (details) {
-    detailsContainer.innerHTML += `<div class="film-title">
-            <h1>${details.name}</h1>
-            <h2>${details.attributes[0].terms[0].name}</h2>
-            <h3>${details.tags.name}</h3>
-          </div>`;
-  });
+function createHTML(details) {
+  detailsContainer.innerHTML += `<div class="film-title">
+                                    <h1>${details.name}</h1>
+                                    <h2>${details.attributes[0].terms[0].name}</h2>
+                                    <h3>${details.attributes[1].terms[1].name}</h3>
+                                  </div>
+                                  <div class="film-wrapper">
+                                    <img src=${details.images[1].src}
+                                    alt="" class="film-image" />
+                                      <div class="container film-info">
+                                        <p>${details.description}</p>
+                                        <p>Price: ${details.prices.price} ${details.prices.currency_code}</p>
+                                        
+                                          <div class="button-wrapper__film">
+                                      <a href="checkout.html" class="cta">Watch Now</a>
+                                          </div>
+                                      </div>
+                                  </div>`;
 }
-
-// function createHTML(filmDetails) {
-//     detailsContainer.innerHTML `<h1>${details.name}</h1>`
-// }
-
-// function createHTML(details) {
-//     detailsContainer.innerHTML `<div class="film-title">
-//     <h1>${details.name}</h1>
-//     <h2>${details.attributes[0].terms[0].name}</h2>
-//     <h3>${details.tags.name}</h3>
-//   </div>`
-
-// }
