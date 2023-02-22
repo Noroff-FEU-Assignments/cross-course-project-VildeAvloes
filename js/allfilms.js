fe550486e6a1fb565c5e05ba33a3e650bef9971a
VildeAvloes <vildeavlos@gmail.com>
2023-02-22 16:09:20 +0100

    updated  allfilms

--- js/allfilms.js
+++ js/allfilms.js
@@ -4,22 +4,22 @@ const filmResultsContainer = document.querySelector(".film-results");
 async function getFilms() {
   try {
     const response = await fetch(url);
-    const getResults = await response.json();
-    createHTML(getResults);
-    console.log(getResults);
+    const filmResults = await response.json();
+
+    console.log(filmResults);
+
+    filmResultsContainer.innerHTML = "";
+
+    filmResults.forEach(function (film) {
+      filmResultsContainer.innerHTML += `<div class="card-wrapper">
+                                            <a href="film-preview.html?id=${film.id}" class="card">
+                                            <img src="${film.images[0].src}" 
+                                            alt="${film.images[0].alt}" class="preview-image" />${film.name}</a>
+                                          </div>`;
+    });
   } catch (error) {
     console.log(error);
   }
 }
 
 getFilms();
-
-function createHTML(films) {
-  films.forEach(function (film) {
-    filmResultsContainer.innerHTML += `<div class="card-wrapper">
-                                          <a href="film-preview.html?id=${film.id}" class="card">
-                                          <img src="${film.images[0].src}" 
-                                          alt="${film.images[0].alt}" class="preview-image" />${film.name}</a>
-                                        </div>`;
-  });
-}
