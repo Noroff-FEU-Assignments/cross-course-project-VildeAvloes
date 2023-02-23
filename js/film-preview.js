3ea084ab175556725b6c141c364507236cc657d9
VildeAvloes <vildeavlos@gmail.com>
2023-02-23 09:55:22 +0100

    renderes filmGenres

--- js/film-preview.js
+++ js/film-preview.js
@@ -17,35 +17,24 @@ async function getFilmDetails() {
     const details = await response.json();
     const genres = details.tags;
 
-    console.log(details);
-
-    console.log(details.tags);
-
     detailsContainer.innerHTML = "";
 
-    genres.forEach(function (genre) {
-      let filmGenres = genre.name;
-      console.log(filmGenres);
+    const filmGenres = genres.map(function (genre) {
+      return genre.name;
     });
-
-    // for (var i = 0; i < genres.length; i++) {
-    //   console.log(genres[i].name);
-    // }
 
     detailsContainer.innerHTML += `<div class="film-title">
                                         <h1>${details.name}</h1>
                                         <h2>${details.attributes[0].terms[0].name}</h2>
-                                        <h3>${details.filmGenres}</h3>
+                                        <h3>${filmGenres}</h3>
                                       </div>
                                       <div class="film-wrapper">
                                         <img src=${details.images[1].src}
                                         alt="${details.images[1].alt}" class="film-image" />
                                           <div class="container film-info">
                                             <p>${details.description}</p>
-                                            <p>Price: ${details.prices.price} ${details.prices.currency_code}</p>
-    
                                               <div class="button-wrapper__film">
-                                          <a href="checkout.html" class="cta">Watch Now</a>
+                                          <a href="checkout.html" class="cta">Watch Now for ${details.prices.price} ${details.prices.currency_code}</a>
                                               </div>
                                           </div>
                                       </div>`;
